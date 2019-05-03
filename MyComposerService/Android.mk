98f6911578e8ba72e855b84d10721576e8b3119e
Wei.Liu <xiruanliuwei@163.com>
2019-05-02 20:24:46 -0700

    MyComposerService: Add getBuiltInDisplay, createDisplayEventConnection, getDisplayStats testing codes

--- MyComposerService/Android.mk
+++ MyComposerService/Android.mk
@@ -8,12 +8,15 @@ LOCAL_MODULE := MyComposerService
 LOCAL_SRC_FILES := MyComposerService.cpp
 
 LOCAL_C_INCLUDES := \
+	frameworks/native/libs/binder/include \
 	frameworks/native/libs/gui/include \
+	frameworks/native/libs/ui/include \
 	system/core/include
 
 LOCAL_SHARED_LIBRARIES :=
 
 LOCAL_LDLIBS := \
+	-lbinder \
 	-lgui \
 	-llog \
 	-lutils
--- MyComposerService/MyComposerService.cpp
+++ MyComposerService/MyComposerService.cpp
@@ -1,22 +1,72 @@
 
-#ifdef TAG_LOG
-#undef TAG_LOG
+#ifdef LOG_TAG
+#undef LOG_TAG
 #endif
 
-#define TAG_LOG "Antony"
+#define LOG_TAG "Antony"
 
+#include <binder/IBinder.h>
+
+#include <gui/IDisplayEventConnection.h>
 #include <gui/ISurfaceComposer.h>
+#include <gui/ISurfaceComposerClient.h>
 
 #include <log/log_main.h>
 
+#include <private/gui/BitTube.h>
 #include <private/gui/ComposerService.h>
 
-using namespace std;
+#include <ui/DisplayStatInfo.h>
+
+using namespace android;
 
 int main(int argc, char** argv) {
 
     ALOGD("argc is: %d\n", argc);
     ALOGD("argv is: %p\n", argv);
+
+    sp<ISurfaceComposer> composer = ComposerService::getComposerService();
+
+    sp<ISurfaceComposerClient> client = composer.get()->createConnection();
+
+    ALOGD("client.get() is: %p\n", client.get());
+
+    sp<IBinder> main_display = composer.get()->getBuiltInDisplay(ISurfaceComposer::eDisplayIdMain);
+    ALOGD("main_display is: %p\n", main_display.get());
+
+    sp<IBinder> hdmi_display = composer.get()->getBuiltInDisplay(ISurfaceComposer::eDisplayIdHdmi);
+    ALOGD("hdmi_display is: %p\n", hdmi_display.get());
+
+    {
+        sp<IDisplayEventConnection> connection = composer.get()->createDisplayEventConnection(
+            ISurfaceComposer::eVsyncSourceSurfaceFlinger
+            );
+
+        ALOGD("connection.get() is: %p\n", connection.get());
+
+        gui::BitTube bt;
+
+        if(nullptr != connection.get()) {
+            connection.get()->stealReceiveChannel(&bt);
+
+            ALOGD("BitTube, getFd is: %d\n", bt.getFd());
+            ALOGD("BitTube, getSendFd is: %d\n", bt.getSendFd());
+
+        }
+    }
+
+    {
+        status_t result = 0;
+
+        DisplayStatInfo stats;
+
+        result = composer.get()->getDisplayStats(main_display, &stats);
+
+        ALOGD("stats.vsyncTime is: %lld\n", (long long int)(stats.vsyncTime));
+        ALOGD("stats.vsyncPeriod is: %lld\n", (long long int)(stats.vsyncPeriod));
+    }
+
+    
 
     return 0;
 }
